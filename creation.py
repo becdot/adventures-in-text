0a19a4343198eb6e3151fb6edbaaf60561110548
Becca Liss <becca.liss@gmail.com>
2013-02-26 17:00:44 -0500

    Dynamic object creation

---
+++ creation.py
@@ -0,0 +1,39 @@
+from scaffolding import Object
+from properties import Openable, Lightable, UnreachableLight, Gettable, Climbable, Container
+
+def validate_attrs(bases):
+    def decorator(init):
+        def decorated(self, *args, **kwargs):
+            init(self, *args, **kwargs)
+            failed = []
+            for base in bases:
+                for required_attr in base.required_attrs:
+                    try:
+                        getattr(self, required_attr)
+                    except AttributeError:
+                        failed.append(required_attr)
+            if failed:
+                raise Exception("Missing required attributes: {}".format(', '.join(failed)))
+        return decorated
+    return decorator
+
+
+def create_object(bases=(), attributes={}, methods={}):
+    name = attributes['name'].title()
+
+    @validate_attrs(bases)
+    def init(self, *args, **kwargs):
+        for k, v in attributes.items():
+            setattr(self, k, v)
+
+    methods['__init__'] = init
+
+    return type(attributes['name'], bases, methods)
+
+
+# candledict = {'bases': (Lightable, Object), 'attributes':{'name': 'candle', 'id': 'candle', 'is_lit': False, 'description': 'A candle.',
+#                 'on_description': 'It turns on.', 'off_description': 'It turns off.'}}
+
+# Candle = create_object(**candledict)
+
+# candle = Candle()
